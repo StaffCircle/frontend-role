e7fcd3c663d8d21a1a3de175283699f2ab6cceab
Neil Merton <neil.merton@staffcircle.com>
2020-03-20 20:30:43 +0000

    - UI improvements - Include Benefits

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,8 +31,15 @@
       <li>Actively participate in project scoping, planning, design and execution</li>
       <li>Use your knowledge of frontend engineering concepts to design and implement tested, reusable, and modular code</li>
       <li>Apply knowledge of the business goals to derive innovative solutions for organisation and our clients</li>
-      <li>Translating designs and wireframes into high quality component-based code</li>
+      <li>Translating designs &amp; wireframes into interactive <abbr title="Proof of concept">POC</abbr>s, then high quality, reusable component-based code</li>
       <li>Solid understanding on modular design and single responsibility principle</li>
+    </ul>
+    <h2>Benefits</h2>
+    <ul>
+      <li>Remote working</li>
+      <li>Flexible working hours</li>
+      <li>Competitive salary</li>
+      <li>Regular events and conferences</li>
     </ul>
     <app-alert title="A note to Recruitment Agencies">
       We don't accept speculative CVs. Any unsolicited CVs received will be treated as the property of StaffCirlce Ltd and any Terms &amp; Conditions associated with the use of such CVs will be considered <code>null</code> and <code>void</code>.
